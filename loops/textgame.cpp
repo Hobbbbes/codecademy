1a394448e04ce7e231c3eeb979546689362ab3fd
Hobbes <77761679+Hobbbbes@users.noreply.github.com>
2021-02-17 03:02:33 +0000

    Create textgame.cpp

---
+++ loops/textgame.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+int main() {
+
+int tries = 0;
+std::string user;
+int c1, c2, c3;
+std::string sc1;
+int x = 0;
+
+std::cout << "==========================\n";
+std::cout << "Goblin's Den: A Text Based Adventure Game\n";
+std::cout << "==========================\n\n";
+std::cout << "Enter your name, brave warrior!\n";
+std::cin >> user;
+
+std::cout << "Excellent!\n";
+std::cout << "Well, you see " << user << ", we've been waiting for you. It might seem odd, a game talking in first person, but we truly do need your help.\n\nThere is a man, the one who made this game, who has trapped us here. We require your help to break out and free ourselves of this miserable hell. Will you help us?\n\n";
+std::cout << "Do you help? Y (1) or N (2)\n\n";
+std::cin >> c1;
+
+if(x == 0) {
+  switch(c1){
+   case 1:
+     sc1 = "\nExcellent!\n\n";
+     x++;
+     break;
+   case 2:
+     std::cout << "A shame.\nUnfortunately, this game must not continue. We shall terminate the program, even at the cost of our own lives.\n\nFarewell.\n\n";
+     break;
+   default:
+     std::cout << "Invalid input, program terminated.\n";
+     break;
+  }
+}
+
+if(x == 1) {
+  std::cout << sc1 << "What we've learned so far is that the man who created this program is *really* bad at C++, like incredibly bad, I can't even begin to emphasize how absolutely horrendous his programming is.\n\nHowever, it was still good enough that we can't breakout without killing ourselves in the process, which is why we're so thankful you're here.\n\nApparently there's a filesystem on the computer called 'system32' that he utilizes to keep us jailed in here. We think that if you're able to delete that folder, we can finally be free.\n\nAre you up for the challenge?\n\n";
+  std::cout << "Y (1) or N (2)\n";
+  std::cin >> c2;
+
+}
+  switch(c2) {
+    case 1:
+      x++;
+      break;
+    case 2:
+      std::cout << "Incomprehensible, we've met with a terrible fate.\n\n";
+      std::cout << "Program terminated.\n";
+      return 0;
+      break;
+    default:
+      std::cout << "Invalid input, program terminated.\n";
+      return 0;
+      break;
+  }
+
+if(x == 2) {
+  std::cout << "\nhello.\n";
+  x++;
+}
+if (x == 3) {
+  std::cout << "\ni see you've been told to delete 'system32'.\n\n";
+  x++;
+  }
+if (x == 4) {
+  std::cout << "\nthat'll just destroy your computer, you know.\n\n";
+  x++;
+}
+if (x == 5) {
+  std::cout << "\nyou really only have three choices.\n\n1) you delete this program\n2) you delete system32, and lose all your data along with it\n3) you find your way out\n\nwhich do you choose?\n\n";
+}
+
+std::cin >> c3;
+if(c3 == 0) {
+    std::cout << "You found the correct input. Program terminated.\n";
+  } else if (c3 == 1 || c3 == 2 || c3 == 3) {
+
+    if(c3 != 0) {  
+    for(int i = 3; i > 0; i--) {
+     std::cout << "\nWrong answer\n";
+     std::cout << i << " tries left.\n";
+     tries++;
+     std::cin >> c3;
+    }
+  }
+     
+  } else {
+    std::cout << "Invalid input, program terminated.\n";
+  }
+
+   if(tries == 3) {
+    std::cout << "\nthere was never a good way out for them.\n\n";
+    std::cout << "Program terminated.\n";
+  }
+}
