a3f963ac96b62a4cdc3f6971c7eb57e7c47947a4
Hobbes <77761679+Hobbbbes@users.noreply.github.com>
2021-03-02 20:23:38 +0000

    Create ufo_func.hpp

---
+++ functions/hangman/ufo_func.hpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+
+void greet();
+void display_misses(int misses);
+void display_status(std::string answer, std::vector<char> incorrect);
+void end_game(std::string answer, std::string codeword);
+
+// Declare functions
