4bd6de97e74289c028fc00700bc27172342163d4
Hobbes <77761679+Hobbbbes@users.noreply.github.com>
2021-03-02 20:31:53 +0000

    Update ufo_func.hpp

--- functions/hangman/ufo_func.hpp
+++ functions/hangman/ufo_func.hpp
@@ -1,9 +1,10 @@
 #include <vector>
 #include <string>
 
+// Function declaration so our main program knows what the paramaters are for our given functions
 void greet();
 void display_misses(int misses);
 void display_status(std::string answer, std::vector<char> incorrect);
 void end_game(std::string answer, std::string codeword);
 
-// Declare functions
+
